e7c20143e3ae469a4515cd5d8ddee2e56f9069a2
4ntfer <profissionalantoniofferreira@gmail.com>
2023-05-31 11:09:06 -0300

    O funcionamento interno das portas AND e OR está funcionando

--- src/Node.js
+++ src/Node.js
@@ -1,13 +1,22 @@
+import {AndPort} from "./AndPort.js";
+import {OrPort} from "./OrPort.js";
+import { TruePort } from "./TruePort.js";
+import {FalsePort} from "./FalsePort.js"
 
 export class Node{
     Linput;
     Rinput;
     port;
 
-    Node(Linput, Rinput, port){
+    constructor(Linput, Rinput, port){
         this.Linput = Linput;
         this.Rinput = Rinput;
-        port.setInputs(Linput.getResult(), Rinput.getResult());
+        port.setInput(Linput.getOutput(), Rinput.getOutput());
         this.port = port;
+        console.log(typeof(port));
+    }
+
+    getOutput(){
+        return this.port.result();
     }
 }

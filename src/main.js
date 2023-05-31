96727bb8420953007ae021407b3671c456687892
4ntfer <profissionalantoniofferreira@gmail.com>
2023-05-31 11:15:59 -0300

    Alguns arquivos fora renomeados

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
-export class main{
+
+import { FalsePort } from "./FalsePort.js";
+import {Node} from "./Node.js";
+import { TruePort } from "./TruePort.js";
+import { AndPort } from "./AndPort.js"; 
+import { OrPort } from "./OrPort.js"; 
+
+export class Main{
     start(){
-        let G = [] // vetor de node
+        let G = []; // vetor de node
+        G.push(new Node(new FalsePort(), new FalsePort, new OrPort()));
+        console.log(G[0].getOutput());
     }
 }

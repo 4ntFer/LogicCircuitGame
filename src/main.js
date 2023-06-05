0a280b04ed70c0aec361c93760e3e07c6e02cbf1
4ntfer <profissionalantoniofferreira@gmail.com>
2023-06-05 13:43:16 -0300

    Foram adicionados recursos visuis para visualizar o conteudo da arvore

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ export class Main{
     context;
     canvaWidth =  600;
     canvaHeight = 400;
+    raio = 30;
 
     constructor(cntx){
         this.context =  cntx;
@@ -19,25 +20,60 @@ export class Main{
         const FalsePortNode = new Node(null, null, new FalsePort);
 
         let G = []; // vetor de node
-        G.push(new Node(FalsePortNode, FalsePortNode, new OrPort()));
+        G.push(new Node(TruePortNode, TruePortNode, new AndPort()));
         
-
-        this.paintTree(G.pop(), this.canvaWidth/2, 10);
+        
+        this.paintGameBoard(G.pop());
     }
 
-    paintTree(head, x, y){
-        const raio = 30;
+    paintGameBoard(head){
+        const x = this.canvaWidth/2;
+        const y = this.raio;
+        const raio = this.raio
         const bord = 4*raio
+        const diametro = raio*2
 
         this.context.beginPath();
         this.context.arc(x, y + raio, raio, 0, Math.PI/2, true);
         this.context.stroke();
+
+        console.log(head.getOutput());
+        this.context.fillText(head.getOutput(), x - raio / 2 , y + raio);
+
+        this.paintLine(x, y + diametro, this.canvaWidth/2, y + bord);
+
+        this.paintTree(head, this.canvaWidth/2, y + bord);
+    }
+
+    paintTree(head, x, y){
+        const raio = this. raio;
+        const diametro = raio*2;
+        const bord = 4*raio
+
+        let xInitLine = x;
+        let yInitLine = y + diametro
+
+        this.context.beginPath();
+        this.context.arc(x, y + raio, raio, 0, Math.PI/2, true);
+        this.context.stroke();
+        
+
+        this.context.fillText(head.port.id, x - raio / 2 , y + raio);
+
         if(head.Linput != null){
+            this.paintLine(xInitLine - 30 , yInitLine - 15, xInitLine - bord, yInitLine + bord/2);
             this.paintTree(head.Linput, x - bord, y + bord);
         }
 
         if(head.Rinput != null){
+            this.paintLine(xInitLine + 30, yInitLine - 15, xInitLine + bord, yInitLine + bord/2);
             this.paintTree(head.Rinput, x + bord, y + bord);
         }
-    } 
+    }
+
+    paintLine(x1, y1, x2, y2){
+        this.context.moveTo(x1, y1);
+        this.context.lineTo(x2, y2);
+        this.context.stroke();
+    }
 }
